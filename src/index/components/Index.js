738f826a4bdaaf09388bd6b3a28856b86d8e8230
Rui Oliveira <rui.oliveira@wegoshare.com>
2020-09-29 16:53:49 +0100

    Remove unused imports

--- src/index/components/Index.js
+++ src/index/components/Index.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react'
-import { Icon } from 'src/lib/components/Icon'
-import { colors } from 'src/colors'
-import { Typography } from 'src/lib/components/Typography'
 import { Divider } from 'material-ui'
-import { CircledIcon } from 'src/lib/components/CircledIcon'
-
-import { cn } from 'src/index/components/Index.style'
 
 export class Index extends Component {
   render() {
