1aa398203c57aa8fbc6ced63d05d7f3cba9ac4a8
Rui Oliveira <rui.oliveira@wegoshare.com>
2020-09-29 12:21:42 +0100

    Revert "Fix add token in local storage"
    This reverts commit c30916a087078848e254d64a452dc5619248c581.

--- src/lib/services/Auth.js
+++ src/lib/services/Auth.js
@@ -3,7 +3,7 @@ import store from 'store'
 class Auth {
   set(isSet, info = undefined) {
     store.set('isAuthSet', isSet)
-    store.set('token', info.id)
+    info !== undefined ? store.set('token', info.id) : store.remove('token')
   }
 
   isSet() {
